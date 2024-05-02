d80ac5792ddd90faeb0dc7a908b6384ee59b2e52
Anna Menitskaya <menitskaya@gmail.com>
2024-05-01 19:16:54 -0700

    added fix for redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,5 @@
-'use client'
-
-import { useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  const router = useRouter()
-  router.push('/template1')
-  return (
-    <div>Redirecting...</div> // Placeholder content while redirecting
-  )
+  redirect('/template1')
 }
-
-
-
